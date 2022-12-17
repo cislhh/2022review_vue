d1b328fb74b97f46ab1322cef2619cafdbeda61c
Azo <1353761785@qq.com>
2022-12-17 19:06:25 +0800

    拓展依赖，更新vscode

---
+++ vue3.x/vue3.xDemo/src/components/demoIndex.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        {{ vueMsg }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import {} from "vue";
+const vueMsg: string = "123";
+
+</script>
+
+<style lang="" scoped></style>
